d408cbe8c61152a43f4d9b98a5d2b0410e28d853
Rayhan Sheikh <rayhan.sheikh10@gmail.com>
2024-02-27 17:48:39 +0600

    error git

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,12 +2,12 @@
 import React from 'react'
 import Product from './Product';
 
-const ProductList = ({ products, searchResult }) => {
+const ProductList = ({ products, searchResult, searchTxt }) => {
   return (
     <div className="grid grid-cols-4 2xl:grid-cols-5 gap-2">
-      {searchResult?.length ?
+      {(searchResult?.length >=0 && searchTxt) ?
         <>
-          {searchResult.map((product, i) => {
+          {searchResult?.map((product, i) => {
             return <Product key={i} product={product} />;
           })}
         </> :
