6234c6c93a4008881b06fa65167a3e02054e7953
Rayhan Sheikh <rayhan.sheikh10@gmail.com>
2024-02-28 23:45:28 +0600

    Finished the app

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -10,7 +10,7 @@ import CartBottom from "./CartBottom";
 import CartTotal from "./CartTotal";
 import MobileCart from "./MobileCart";
 
-const CartList = ({open}) => {
+const CartList = ({ open }) => {
 
     const cartItems = useSelector((state) => state.cart.items);
 
@@ -22,26 +22,29 @@ const CartList = ({open}) => {
     return (
         <div className="relative h-full">
             {(cartItems && cartItems.length) > 0 ?
-                <div className="overflow-x-auto lg:mt-5 overflow-y-auto">
-                    <table className="w-full hidden lg:block">
-                        <tbody>
-                            {
-                                cartItems?.map((item, id) => {
-                                    return <tr key={id} className="text-slate-500">
-                                        <td className="text-slate-500 py-3 w-10"><button><FaRegEdit className="text-2xl text-slate-500" /></button></td>
-                                        <td className="text-slate-500 font-semibold border border-r-0 px-4 py-3 truncate max-w-60">{item.title}</td>
-                                        <td className="text-slate-500 font-semibold border-y px-4 py-3">${item?.price}</td>
-                                        <td className="text-slate-500 font-semibold border-y px-4 py-3 w-5 text-right "><button onClick={() => dispatch(decrementQty(item?.id))}><FaMinusCircle className="text-2xl text-slate-500 font-semibold" /></button></td>
-                                        <td className="border-y px-4 py-3 w-3 text-slate-500 font-semibold">{item?.qty}</td>
-                                        <td className="border-y px-4 py-3 w-5 text-slate-500 font-semibold"><button onClick={() => dispatch(incrementQty(item?.id))}><FaPlusCircle className="text-2xl text-slate-500 font-semibold" /></button></td>
-                                        <td className="border border-l-0 px-4 py-3 text-right w-10 text-slate-500 font-semibold">${(item?.price * item?.qty).toFixed(2)}</td>
-                                        <td className="py-3 w-8 text-slate-500 text-right"><button onClick={() => handleRemoveCartItem(item.id)}><RiDeleteBin5Line className="text-2xl text-red-500" /></button></td>
-                                    </tr>
-                                })}
-                        </tbody>
-                    </table>
+                <>
+                    <div className="overflow-x-auto lg:mt-5 hidden lg:block">
+                        <table className="w-full ">
+                            <tbody>
+                                {
+                                    cartItems?.map((item, id) => {
+                                        return <tr key={id} className="text-slate-500">
+                                            <td className="text-slate-500 py-3 w-10"><button><FaRegEdit className="text-2xl text-slate-500" /></button></td>
+                                            <td className="text-slate-500 font-semibold border border-r-0 px-4 py-3 truncate max-w-60">{item.title}</td>
+                                            <td className="text-slate-500 font-semibold border-y px-4 py-3">${item?.price}</td>
+                                            <td className="text-slate-500 font-semibold border-y px-4 py-3 w-5 text-right "><button onClick={() => dispatch(decrementQty(item?.id))}><FaMinusCircle className="text-2xl text-slate-500 font-semibold" /></button></td>
+                                            <td className="border-y px-4 py-3 w-3 text-slate-500 font-semibold">{item?.qty}</td>
+                                            <td className="border-y px-4 py-3 w-5 text-slate-500 font-semibold"><button onClick={() => dispatch(incrementQty(item?.id))}><FaPlusCircle className="text-2xl text-slate-500 font-semibold" /></button></td>
+                                            <td className="border border-l-0 px-4 py-3 text-right w-10 text-slate-500 font-semibold">${(item?.price * item?.qty).toFixed(2)}</td>
+                                            <td className="py-3 w-8 text-slate-500 text-right"><button onClick={() => handleRemoveCartItem(item.id)}><RiDeleteBin5Line className="text-2xl text-red-500" /></button></td>
+                                        </tr>
+                                    })}
+                            </tbody>
+                        </table>
+                    </div>
                     <MobileCart cartItems={cartItems} />
-                </div> :
+                </>
+                :
                 <div className="py-5">
                     <p className="text-2xl text-slate-500 text-center font-semibold">No product in your cart</p>
                 </div>}
